62b3d215346a8b77ce66666ac61e196f27f8e02a
AbhimanyuAryan <abhimanyu.chat@gmail.com>
2017-01-29 16:41:47 +0530

    First App

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using System; 
+using Microsoft.EntityFrameworkCore; 
+
+namespace ForgingAhead.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+    }
+}
---
+++ Models/Character.cs
@@ -0,0 +1,12 @@
+namespace ForgingAhead.Models
+{
+    public class Character
+    { 
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+        public int Level { get; set; }
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+    }
+}
